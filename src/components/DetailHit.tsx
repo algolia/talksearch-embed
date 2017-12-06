277710ad734df6ed0c8a93fe8c0fd86fd1e25bdd
Haroen Viaene <hello@haroen.me>
2017-12-06 18:24:53 +0100

    extract

--- src/components/DetailHit.tsx
+++ src/components/DetailHit.tsx
@@ -1,36 +1,7 @@
 import { h, Component } from 'preact';
-import { Highlight } from 'react-instantsearch/dom';
-import secToMin from 'sec-to-min';
+import TranscriptMatch from './TranscriptMatch';
+import SeekButton from './SeekButton';
 import { SingleHit } from '../App';
-
-const TranscriptMatch = ({ hit }: { hit: SingleHit }) => (
-  <Highlight hit={hit} attributeName="text" tagName="mark" />
-);
-
-const Time = ({ time }: { time: number }) => <span>{secToMin(time)}</span>;
-export const SeekButton = ({
-  start,
-  onClick,
-}: {
-  start: number;
-  onClick: () => void;
-}) => (
-  <button
-    className="text mr2"
-    style={{
-      width: '36px',
-      height: '36px',
-    }}
-    onClick={onClick}
-  >
-    <div className="fla flc ma1 f5">
-      <span className="icon-play tac" />
-    </div>
-    <div className="fln f7 tc">
-      <Time time={start} />
-    </div>
-  </button>
-);
 
 interface HitProps {
   hit: SingleHit;
@@ -43,13 +14,9 @@ export default class Hit extends Component<HitProps, any> {
     const { hit } = this.props;
     const { start } = hit;
     return (
-      <div className="flrnw mb2 items-center">
+      <div className="flex mb2 items-center">
         <SeekButton onClick={this.onSeek} start={start} />
-        <div className="fla flcnw flccv">
-          <div className="fln f6">
-            <TranscriptMatch hit={hit} />
-          </div>
-        </div>
+        <TranscriptMatch hit={hit} />
       </div>
     );
   }
