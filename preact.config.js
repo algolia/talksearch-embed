644ed149177bebe23af630671f1234fd93ad70b3
Haroen Viaene <hello@haroen.me>
2017-12-06 11:04:39 +0100

    make assets work

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,11 @@
 import preactCliTypeScript from 'preact-cli-plugin-typescript';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 /* eslint-disable no-param-reassign */
 export default config => {
   preactCliTypeScript(config);
   config.node.process = 'mock';
+  config.plugins.push(
+    new CopyWebpackPlugin([{ context: `${__dirname}/assets`, from: `*.*` }])
+  );
 };
