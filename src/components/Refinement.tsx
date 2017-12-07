eca5fbb7a39280389e0c343c6cef1d0344dacf09
Haroen Viaene <hello@haroen.me>
2017-12-07 12:04:54 +0100

    rsponsive

--- src/components/Refinement.tsx
+++ src/components/Refinement.tsx
@@ -11,7 +11,7 @@ export default class Refinement extends Component<Props, void> {
   render() {
     const { attribute } = this.props;
     return (
-      <article className="fl ml1 w-100 w-30-m w-20-l">
+      <article className="fl w-100-40r w-30-m w-20-l">
         <h1 className="hide-40 f5">{attribute}</h1>
         <Media query="(min-width: 40rem)">
           {matches => (
