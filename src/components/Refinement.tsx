230ea7c8ead3d7a45ed3fa31e9b715bc0979bced
Haroen Viaene <hello@haroen.me>
2017-12-07 11:48:07 +0100

    inbetween

--- src/components/Refinement.tsx
+++ src/components/Refinement.tsx
@@ -11,8 +11,8 @@ export default class Refinement extends Component<Props, void> {
   render() {
     const { attribute } = this.props;
     return (
-      <article className="ml1 w-100 w-30-m w-20-l pr2">
-        <h1 className="f5">{attribute}</h1>
+      <article className="fl ml1 w-100 w-30-m w-20-l">
+        <h1 className="hide-40 f5">{attribute}</h1>
         <Media query="(min-width: 40rem)">
           {matches => (
             <RefinementList attributeName={attribute} withSearchBox={matches} />
