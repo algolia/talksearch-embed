b2667297d7e65776995c60d17300543bbb265af3
Haroen Viaene <hello@haroen.me>
2017-12-07 09:47:14 +0100

    flexible header

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default class SearchBar extends Component<Props, void> {
   render() {
     return (
-      <div className="flex items-center ma2 mb3">
+      <div className="flex flex-wrap flex-nowrap-ns items-center ma2 mb3">
         <span className="ma3">{this.props.name}</span>
         <SearchBox />
         <div className="ma3">
