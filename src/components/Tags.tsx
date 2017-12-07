b98d9ead547a46f0aa86a65c3ce8260a6dcad0ff
Haroen Viaene <hello@haroen.me>
2017-12-07 17:39:51 +0100

    auto overflow

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -9,7 +9,7 @@ export default class Tags extends Component<Props, void> {
   render() {
     const { tags = [], onRefine } = this.props;
     return (
-      <div className="flex overflow-scroll f7">
+      <div className="flex overflow-auto f7">
         {tags.map(tag => (
           <button
             className="bn nowrap bg-logan fw3 ma1 pa1 br-pill pointer"
