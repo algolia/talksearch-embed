4e9cd74d9a0bd0aeb4c1634185dfdc4640b8b795
Haroen Viaene <hello@haroen.me>
2017-12-07 08:45:57 +0100

    fetch metadata

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Component } from 'preact';
+import algoliasearch from 'algoliasearch';
 import applyPolyfills from './util/polyfill';
 import App from './App.tsx';
 import Error from './Error.tsx';
@@ -14,8 +15,27 @@ if (typeof window !== 'undefined') {
   document.body.style.setProperty('--color', themeColor);
 }
 
+const client = algoliasearch('FOQUAZ6YNS', '72ee3a317835b8618eda01c6fcc88f77');
+const index = client.initIndex('METADATA');
+
 export default class Index extends Component {
+  state = {
+    metadata: {},
+  };
+
+  async componentDidMount() {
+    if (indexName) {
+      this.setState({
+        metadata: await index.getObject(indexName),
+      });
+    }
+  }
+
   render() {
-    return indexName === null ? <Error /> : <App indexName={indexName} />;
+    return indexName === null ? (
+      <Error />
+    ) : (
+      <App indexName={indexName} metadata={this.state.metadata} />
+    );
   }
 }
