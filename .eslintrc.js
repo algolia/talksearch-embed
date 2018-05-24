79f248c733481409a9064d06d8c7b5bbd3d5147b
Pixelastic <tim@pixelastic.com>
2018-05-24 18:17:22 +0200

    Create playground

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['algolia', 'algolia/jest'],
+  globals: {
+    '__rewire_reset_all__': true,
+    'instantsearch': true
+  },
+  rules: {
+    'no-console': 0
+  }
+};
---
+++ playground/search.js
@@ -0,0 +1,8 @@
+const search = instantsearch({
+  appId: 'latency',
+  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName: 'instant_search',
+  routing: true,
+});
+
+search.start();
