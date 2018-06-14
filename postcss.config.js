4e03f59dd0550636a4e18e9facf06814f0a2f3dc
Pixelastic <tim@pixelastic.com>
2018-06-14 10:46:00 +0200

    Adding js and css builds

---
+++ postcss.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable import/no-commonjs */
+const _ = require('lodash');
+const tailwind = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const filterRules = require('postcss-filter-rules');
+const cleancss = require('postcss-clean');
+const shouldMinify = process.env.MINIFY;
+
+const tailwindConfig = './tailwind.config.js';
+const filterOptions = {
+  filter(selector) {
+    return _.startsWith(selector, '.ais-');
+  },
+};
+const cleanCssOptions = {
+  level: {
+    1: {
+      specialComments: false,
+    },
+  },
+};
+
+const pluginList = [
+  tailwind(tailwindConfig), // Load tailwind utilities
+  filterRules(filterOptions), // Keep only .ais-* classes
+  autoprefixer, // Crossbrowser compat
+];
+
+if (shouldMinify) {
+  pluginList.push(cleancss(cleanCssOptions));
+}
+
+module.exports = {
+  plugins: pluginList,
+};
